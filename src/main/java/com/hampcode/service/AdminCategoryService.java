a12854c1d055f92e901117f4eef05bb5a0855385
HampCode <pcsiheme@upc.edu.pe>
2024-09-01 17:14:59 -0500

    feat(backend): implementación del CRUD de categoria

--- src/main/java/com/hampcode/service/AdminCategoryService.java
+++ src/main/java/com/hampcode/service/AdminCategoryService.java
@@ -1,5 +1,16 @@
 package com.hampcode.service;
 
+import com.hampcode.model.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface AdminCategoryService {
-    void hello();
+    List<Category> getAll();
+    Page<Category> paginate(Pageable pageable);
+    Category findById(Integer id);
+    Category create(Category category);
+    Category update(Integer id, Category updateCategory);
+    void delete(Integer id);
 }
